675f2621169e043dd8735c017c5f7d45ec5b162a
Alexandre Pauwels <8890201+ajpauwels@users.noreply.github.com>
2018-09-21 20:18:41 -0400

    Save all executables to dev dependencies

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-// Third-party libs
-var express_1 = require('express');
-// Get the express router
-var router = express_1.Router();
-router.get('/', function (req, res) {
-    res.send('Up and running');
-});
-exports["default"] = router;
